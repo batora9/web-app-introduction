d72f74f5f59d8488662520c064172d4076705f92
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-07-04 15:19:31 +0900

    すべてのnode_modulesを無視

---
+++ connection/server/server.js
@@ -0,0 +1,83 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+
+app.use(cors({ origin: "*" }));
+
+const todoList = [
+  { id: "1", title: "JavaScriptを勉強する", completed: false },
+  { id: "2", title: "TODOアプリを自作する", completed: false },
+  { id: "3", title: "漫画を読み切る", completed: true },
+  { id: "4", title: "ゲームをクリアする", completed: false },
+];
+
+let currentId = 1;
+
+app.get("/api/todo", (c) => c.json(todoList, 200));
+
+app.post("/api/todo", async (c) => {
+  const param = await c.req.json();
+  console.log(param);
+
+  if (!param.title) {
+    throw new Error("Title must be provided");
+  }
+
+  const newTodo = {
+    id: String(currentId++),
+    completed: !!param.completed,
+    title: param.title,
+  };
+
+  todoList.push(newTodo);
+
+  return c.json({ message: "Successfully created" }, 200);
+});
+
+app.put("/api/todo/:id", async (c) => {
+  const param = await c.req.json();
+  console.log(param);
+  const id = c.req.param("id");
+
+  if (!param.title && param.completed === undefined) {
+    throw new Error("Either title or completed must be provided");
+  }
+
+  const todo = todoList.find((todo) => todo.id === id);
+  if (!todo) {
+    throw new Error("Failed to update task title");
+  }
+
+  if (param.title) {
+    todo.title = param.title;
+  }
+
+  if (param.completed !== undefined) {
+    todo.completed = param.completed;
+  }
+
+  return c.json({ message: "Task updated" }, 200);
+});
+
+app.delete("/api/todo/:id", async (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  const todoIndex = todoList.findIndex((todo) => todo.id === id);
+  if (todoIndex === -1) {
+    throw new Error("Failed to delete task");
+  }
+  todoList.splice(todoIndex, 1);
+
+  return c.json({ message: "Task deleted" }, 200);
+});
+
+app.onError((err, c) => {
+  return c.json({ message: err.message }, 400);
+});
+
+serve({
+  fetch: app.fetch,
+  port: 8000,
+});
